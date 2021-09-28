702c8c9c30e93b18b9c0ef1b86d40eeec98b746d
RFuentes22 <00062217@uca.edu.sv>
2021-09-28 08:08:15 -0600

    3 files modified, 1 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -16,16 +14,15 @@ import { CartComponent } from './cart/cart.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
+
     ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductDetailsComponent,
     ProductAlertsComponent,
+
   ],
   bootstrap: [AppComponent],
 })
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,16 +1,13 @@
-import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
 
+export class ProductAlertsComponent {
   @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
-
-  ngOnInit() {
-  }
-
 }
